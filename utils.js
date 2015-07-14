f2e6758740c873108beba3c081c326c9e5f6c69f
Denys Bykanov <denys.bykanov@globallogic.com>
2015-07-14 11:36:12 +0300

    fixed utils

--- utils.js
+++ utils.js
@@ -1,25 +1,6 @@
 var Helper = require('./Helper');
 
 module.exports = {
- 
-     isArray:function (argument) {
-       if(!argument || Object.prototype.toString.call(argument).toUpperCase() !== '[OBJECT ARRAY]')
-        return false;
-       return true;
-     },
-
-     isFunction:function (argument) {
-       if(Object.prototype.toString.call(argument).toUpperCase() !== '[OBJECT FUNCTION]')
-        return false;
-       return true;
-     },
-
-     isObject:function (argument) {
-       if(Object.prototype.toString.call(argument).toUpperCase() !== '[OBJECT OBJECT]')
-        return false;
-       return true;
-     },
-
     /**
      * Sort given array by provided rule in comparator function
      * @param {Array} list
